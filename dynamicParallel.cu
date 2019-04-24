736b00951ff10a2d005962f6bb206ffdcefbe28a
Alex Laubscher <aclaubscher17@gmail.com>
2019-04-24 19:43:43 -0400

    skeleton for code

--- dynamicParallel.cu
+++ dynamicParallel.cu
@@ -8,20 +8,36 @@
 #include <stdlib.h>
 #define SIZE 1024
 
-int main() {
-    int *a, *b, *c;
+__global__ void VectorAdd(int *a, int *b, int n) {
+    printf("Inside the kernel");
+    int i = threadIdx.X;
 
-    cudaMallocManaged(&a, SIZE * sizeof(int));
-    cudaMallocManaged(&b, SIZE * sizeof(int));
-    cudaMallocManaged(&c, SIZE * sizeof(int));
-    cudaMallocManaged(&d, SIZE * sizeof(int));
+    if (i < n)
+        c[i] = a[i] + b[i];
+}
+
+
+int main() {
+    float *dev_a, *dev_b, *dev_c, *dev_d;
+
+    a = (float *)calloc(SIZE, sizeof(float));
+    b = (float *)calloc(SIZE, sizeof(float));
+    c = (float *)calloc(SIZE, sizeof(float));
+    d = (float *)calloc(SIZE, sizeof(float));
+
+    cudaMalloc((void **) &dev_a, count*sizeof(float));
+    cudaMalloc((void **) &dev_b, count*sizeof(float));
+    cudaMalloc((void **) &dev_c, count*sizeof(float));
+    cudaMalloc((void **) &dev_d, count*sizeof(float));
+
 
     for (int i = 0; i < SIZE; ++i) {
         a[i] = i;
         b[i] = i;
-        c[i] = 0;
-        d[i] = 0;
     }
 
-    VectorAdd
+
+
+    free(a), free(b);
+    cudaFree(dev_a), cudaFree(dev_b), cudaFree(dev_c), cudaFree(dev_d);
 }
